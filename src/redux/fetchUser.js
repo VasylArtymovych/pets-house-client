6e6b3d3fa5574a3d6a0f65f14df2a3efd3ba34e6
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2022-11-14 01:41:16 +0300

    Notice+Selectors

--- src/redux/fetchUser.js
+++ src/redux/fetchUser.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const userApi=createApi({
     reducerPath:'userApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:8888/api/auth',
+        baseUrl: 'http://localhost:8888/api',
         // prepareHeaders:(headers, {getState})=>{
         //     const token= getState().users.token
         //     if(token){ headers.set('Athorization', `Bearer ${token}`)}
@@ -13,24 +13,24 @@ export const userApi=createApi({
     endpoints: builder=>({
         registrationUser: builder.mutation({
             query:(payload)=>({
-                url: '/register',
+                url: '/auth/register',
                 method: 'POST',
                 body: payload,
             }),
         }),
         logIn: builder.mutation({
             query:(payload)=>({
-                url: '/login',
+                url: '/auth/login',
                 method: 'POST',
                 body: payload,
             }),
         }),
         getCurrentUser: builder.query({
-            query: ()=> '/current'
+            query: ()=> '/user/current'
             }),
         logOut:builder.mutation({
             query:()=>({
-                url: '/logout',
+                url: '/auth/logout',
                 method: 'POST'
             })
         })
--- src/redux/sliceAuth.js
+++ src/redux/sliceAuth.js
@@ -133,23 +133,3 @@ const persistConfig = {
 };
 
 export const persistSliceAuth= persistReducer(persistConfig, authSlice.reducer) ;
-
-//Selectors
-
-// export const authSelectors={
-//     getUserEmail: state=> state.users.user.email,
-//     getUserName: state=> state.users.user.name,
-//     getUserCity: state=> state.users.user.city,
-//     getUserPhone: state=> state.users.user.phone,
-//     getToken: state=> state.users.token,
-//     isLogged: state=> state.users.isLogged,
-//     isLoadUser: state=> state.users.loadUser,
-//     isErrorServer: state=> state.users.errorServer,
-//     isErrorRegistration: state=> state.users.errorRegistration,
-// }
-
-const getUserName= state=> state.users.user.name
-
-export const authSelectors={
-    getUserName
-}
