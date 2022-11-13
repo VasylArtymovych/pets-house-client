3b3719268cc90c8d28131abe032929357356a00e
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-11-14 01:48:18 +0200

    fixed style and add change inpun

--- src/components/User/UserData/UserData.jsx
+++ src/components/User/UserData/UserData.jsx
@@ -2,14 +2,19 @@ import UserDataItem from '../UserDataItem';
 import scss from './UserData.module.scss';
 import sprite from '../../../images/symbol-defs.svg';
 import DefaultAvatar from '../../../images/desctop/DefaultAvatar.png';
+import { useModal } from 'hooks';
+import Modal from 'components/Modal';
+import UserAvatarModal from '../UserAvatarModal';
 
 const UserData = () => {
+  const { isModalOpen, closeModal, toggleModal } = useModal();
+
   return (
     <div className={scss.data__container}>
       <div className={scss.avatar__container}>
         <img className={scss.data__img} src={DefaultAvatar} alt="avatar" width="233" height="233" />
         <div className={scss.data__box}>
-          <button className={scss.data__btn} type="submit">
+          <button className={scss.data__btn} type="submit" onClick={toggleModal}>
             <svg className={scss.camera} width="18" height="18">
               <use href={sprite + '#icon-profilePhotoCamera'} />
             </svg>
@@ -18,6 +23,11 @@ const UserData = () => {
         </div>
       </div>
       <UserDataItem />
+      {isModalOpen && (
+        <Modal onCloseModal={closeModal}>
+          <UserAvatarModal onCloseModal={closeModal} />
+        </Modal>
+      )}
     </div>
   );
 };
