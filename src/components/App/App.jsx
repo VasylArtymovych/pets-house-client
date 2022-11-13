be60fba355211a3d10bcd5f9fc925a16396725df
VasylArtymovych <art777vasss@gmail.com>
2022-11-13 10:30:55 +0200

    update structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout';
-// import FindPet from 'pages/FindPet';
-// import Login from 'pages/Login';
-// import News from 'pages/News';
-// import OurFriend from 'pages/OurFriend';
 
-// const linkPage = [
-//   { to: '/news', page: <News /> },
-//   { to: '/notices', page: <FindPet /> },
-//   { to: '/friends', page: <OurFriend /> },
-//   { to: '/login', page: <Login /> },
-//   { to: '/register', page: <Register /> }
-// ];
 const Home = lazy(() => import('pages/Home'));
 const Register = lazy(() => import('pages/Register'));
 const UserPage = lazy(() => import('pages/UserPage'));
@@ -34,9 +23,6 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/user" element={<UserPage />} />
-            {/* {linkPage.map((el) => (
-              <Route key={Math.random()} path={el.to} element={el.page} />
-            ))} */}
           </Route>
         </Routes>
       </Suspense>
