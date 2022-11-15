abdb2cc19ed7e7cf19d7d60d76eab7f98fc3cb5f
VasylArtymovych <art777vasss@gmail.com>
2022-11-15 06:54:08 +0200

    update structure

--- src/components/News/NewsFormSearch.jsx
+++ src/components/News/NewsFormSearch.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-import scss from "./News.module.scss"
+import scss from './News.module.scss';
 import { Input, InputForm } from 'components/Input';
 // import { ErrorMessage, Form, Formik } from 'formik';
 
@@ -8,10 +8,10 @@ const FormSearchNews = () => {
     <div className={scss.container}>
       <h2 className={scss.title}>News</h2>
       {/* <InputForm> */}
-      <Input customStyle={scss.InputNews} placeholder={"Search"} />
+      <Input customStyle={scss.InputNews} placeholder={'Search'} />
       {/* </InputForm> */}
     </div>
-  )
-}
+  );
+};
 
 export default FormSearchNews;
