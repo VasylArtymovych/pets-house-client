f8333896f7ab5d14e49d2b580a94db291efb8487
myroslav <myrkozar@gmail.com>
2022-11-16 17:14:38 +0200

    add and fix: home page scroll bar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,11 @@ function Home() {
   return (
     <div className={scss.home__container}>
       <Container>
-        <h1 className={scss.home__title}>Take good care of your small pets</h1>
+        <h1 className={scss.home__title}>
+          <span>Take good care of</span>
+          <br />
+          <span>your small pets</span>
+        </h1>
         <img src={woman} alt="woman" className={scss.home__woman} />
         <img src={heart} alt="heart" className={scss.home__heart} />
       </Container>
