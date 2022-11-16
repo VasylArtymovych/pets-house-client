f7a9130567f3a0bdf9a19523958acc0e3e275f90
VasylArtymovych <art777vasss@gmail.com>
2022-11-16 12:40:51 +0200

    add Container

--- src/pages/FindPet/FindPet.jsx
+++ src/pages/FindPet/FindPet.jsx
@@ -3,15 +3,17 @@ import NoticesSearch from 'components/Notices/NoticesSearch';
 import NoticesCategoriesNav from 'components/Notices/NoticesCategoriesNav';
 import AddNoticeButton from 'components/Notices/AddNoticeButton';
 import NoticesCategoriesList from 'components/Notices/NoticesCategoriesList';
-
+import Container from 'components/Container';
 const FindPet = () => {
   return (
     <div>
-      Find your favourite Pet
-      <NoticesSearch />
-      <NoticesCategoriesNav />
-      <AddNoticeButton />
-      <NoticesCategoriesList />
+      <Container>
+        Find your favourite Pet
+        <NoticesSearch />
+        <NoticesCategoriesNav />
+        <AddNoticeButton />
+        <NoticesCategoriesList />
+      </Container>
     </div>
   );
 };
