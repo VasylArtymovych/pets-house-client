e8e2b28229949aeb1b2dc5a29c9178276c902336
VasylArtymovych <art777vasss@gmail.com>
2022-11-16 14:58:14 +0200

    fix home styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import scss from './Home.module.scss';
 import woman from '../../images/desctop/womanAndDog.png';
 import heart from '../../images/desctop/heart.png';
+import Container from 'components/Container';
 
 function Home() {
   return (
     <div className={scss.home__container}>
-      <h1 className={scss.home__title}>Take good care of your small pets</h1>
-      <img src={woman} alt="woman" className={scss.home__woman} />
-      <img src={heart} alt="heart" className={scss.home__heart} />
+      <Container>
+        <h1 className={scss.home__title}>Take good care of your small pets</h1>
+        <img src={woman} alt="woman" className={scss.home__woman} />
+        <img src={heart} alt="heart" className={scss.home__heart} />
+      </Container>
     </div>
   );
 }
