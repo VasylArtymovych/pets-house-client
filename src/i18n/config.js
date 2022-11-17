f260758dfa5f947d5dc00851daca51a77dcf15e1
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-17 23:08:27 +0200

    add i18files

---
+++ src/i18n/config.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+  fallbackLng: "en",
+  lng: localStorage.getItem("whatLanguage"),
+  resources: {
+    en: {
+      translations: require("./locales/en/translations.json")
+    },
+    ua: {
+      translations: require("./locales/ua/translations.json")
+    }
+    // es: {
+    //   translations: require('./locales/es/translations.json')
+    // }
+  },
+  ns: ["translations"],
+  defaultNS: "translations"
+});
+
+i18n.languages = ["en", "ua", "es"];
+
+export default i18n;
