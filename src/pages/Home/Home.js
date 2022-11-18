02d4a0762a112c874c85dba130d7681f41f77133
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-11-18 22:17:31 +0200

    fix

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,11 @@
-import { useModal } from 'hooks';
-import Modal from 'components/Modal';
-import { ModalAddsPet } from 'components/ModalAddsPet';
-// import { ModalAddsPetSell } from 'components/ModalAddsPetSell';
 
 function Home() {
-  const { isModalOpen, closeModal, toggleModal } = useModal();
-
+  
   return (
     <div>
-      {isModalOpen && (
-        <Modal onCloseModal={closeModal} mode="dark">
-          <ModalAddsPet onCloseModal={closeModal} />
-          {/* <ModalAddsPetSell onCloseModal={closeModal } /> */}
-        </Modal>
-      )}
+    
       Home pages
-      <button type="button" onClick={toggleModal} style={{ width: 40, height: 40, backgroundColor: 'blue' }}></button>
+     
     </div>
   );
 }
