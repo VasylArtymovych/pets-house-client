05004545954bb6ec00f8f2aa9d05e89524cbe6eb
myroslav <myrkozar@gmail.com>
2022-11-19 20:46:02 +0200

    remove: config i18n

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,70 +1,67 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+// import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import Backend from 'i18next-http-backend';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 
-const resources = {
-  en: {
-    translation: {
-      news: 'News',
-      read: 'Read more',
-      friends: 'Our friends',
-      time: 'Time:',
-      adress: 'Address:',
-      email: 'Email:',
-      phone: 'Phone:',
-      login: 'Login',
-      register: 'Register'
-    }
-  },
-  ua: {
-    translation: {
-      news: 'Новини',
-      read: 'наступна сторінка',
-      friends: 'Наші друзі',
-      time: 'Час роботи:',
-      adress: 'Адреса:',
-      email: 'Електронна скринька:',
-      phone: 'Телефон:',
-      login: 'Логін',
-      register: 'Регістр'
-    }
-  }
-};
+// const resources = {
+//   en: {
+//     translation: {
+//       news: 'News',
+//       read: 'Read more',
+//       friends: 'Our friends',
+//       time: 'Time:',
+//       adress: 'Address:',
+//       email: 'Email:',
+//       phone: 'Phone:',
+//       login: 'Login',
+//       register: 'Register'
+//     }
+//   },
+//   ua: {
+//     translation: {
+//       news: 'Новини',
+//       read: 'наступна сторінка',
+//       friends: 'Наші друзі',
+//       time: 'Час роботи:',
+//       adress: 'Адреса:',
+//       email: 'Електронна скринька:',
+//       phone: 'Телефон:',
+//       login: 'Логін',
+//       register: 'Регістр'
+//     }
+//   }
+// };
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    // backend: {
-    //   loadPath: '/assets/locales/{{lng}}/translation.json',
-    // },
+// i18n
+//   .use(Backend)
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
 
-    fallbackLng: 'en',
+//     fallbackLng: 'en',
 
-    lng: localStorage.getItem('whatLanguage'),
+//     lng: localStorage.getItem('whatLanguage'),
 
-    resources,
+//     resources,
 
-    supportedLngs: ['en', 'ua'],
+//     supportedLngs: ['en', 'ua'],
 
-    detection: {
-      order: ['localStorage', 'cookie', 'htmlTag'],
-      lookupLocalStorage: 'i18nextLng',
-      caches: ['localStorage', 'cookie']
-    },
+//     detection: {
+//       order: ['localStorage', 'cookie', 'htmlTag'],
+//       lookupLocalStorage: 'i18nextLng',
+//       caches: ['localStorage', 'cookie']
+//     },
 
-    // debug: true,
+//     debug: true,
 
-    interpolation: {
-      escapeValue: false
-    },
+//     interpolation: {
+//       escapeValue: false
+//     },
 
-    react: {
-      wait: true
-    }
-  });
+//     react: {
+//       wait: true
+//     }
+//   });
 
 export default i18n;
