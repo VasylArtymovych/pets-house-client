572feb1a3ef899ab9f82c3d4cf449cd5daddd4f5
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-20 23:37:44 +0200

    add Translate, sounds

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import style from './Header.module.scss';
 import Container from 'components/Container';
+import ChangeLanguage from 'components/ChangeLanguage';
 const Header = ({ children }) => {
   return (
     <Container>
@@ -8,6 +9,7 @@ const Header = ({ children }) => {
         <nav>
           <div className={style.navWrap}>{children}</div>
         </nav>
+        <ChangeLanguage />
       </header>
     </Container>
   );
