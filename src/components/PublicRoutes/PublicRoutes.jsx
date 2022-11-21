7a6fd73d82e7abce655ce217d00435e2dbe171da
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-21 22:13:22 +0200

    add PublicRoutes

--- src/components/PublicRoutes/PublicRoutes.jsx
+++ src/components/PublicRoutes/PublicRoutes.jsx
@@ -2,10 +2,10 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { selectors } from 'redux/selectors';
 
-export default function PrivateRoutes() {
+export default function PublicRoutes() {
   const { isLogged } = selectors;
   const isLoggedIn = useSelector(isLogged);
   const location = useLocation();
 
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" state={{ location }} replace />;
+  return !isLoggedIn ? <Outlet /> : <Navigate to="/user" state={{ location }} replace />;
 }
