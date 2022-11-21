4d637da0612a58e3f4eb13cda165bdfc21ab2d5d
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-21 17:16:06 +0200

    add sound and some translate

--- src/components/AuthForm/LoginForm.jsx
+++ src/components/AuthForm/LoginForm.jsx
@@ -9,121 +9,76 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { Link } from 'react-router-dom';
 import { loginValidationSchema } from 'services';
+import useSound from 'use-sound';
+
+import dogsound from 'sounds/00990.mp3';
 
 const initialValues = {
-	email: '',
-	password: '',
+  email: '',
+  password: ''
 };
 
-export const LoginForm = props => {
-	const [login] = useLogInMutation();
-	const [isError, setIsError] = useState(null);
-	const [passwordShow, setPasswordShow] = useState(false);
-	const navigate = useNavigate();
+export const LoginForm = (props) => {
+  const [login] = useLogInMutation();
+  const [isError, setIsError] = useState(null);
+  const [passwordShow, setPasswordShow] = useState(false);
+  const navigate = useNavigate();
+  const [play] = useSound(dogsound);
 
-	const togglePassword = () => setPasswordShow(prevState => !prevState);
+  const togglePassword = () => setPasswordShow((prevState) => !prevState);
 
-	const handleSubmit = async (formData, { resetForm }) => {
-		console.log(formData);
-		const { error } = await login(formData);
-		if (error) {
-			setIsError({
-				message: error.data.message,
-				additionalInfo: error.data.additionalInfo,
-			});
-			resetForm();
-		} else {
-			navigate('/user');
-		}
-	};
+  const handleSubmit = async (formData, { resetForm }) => {
+    console.log(formData);
+    const { error } = await login(formData);
+    if (error) {
+      setIsError({
+        message: error.data.message,
+        additionalInfo: error.data.additionalInfo
+      });
+      resetForm();
+    } else {
+      navigate('/user');
+    }
+  };
 
-	return (
-		<div className={scss.container}>
-			<Formik
-				validationSchema={loginValidationSchema}
-				initialValues={initialValues}
-				onSubmit={handleSubmit}
-			>
-				{() => (
-					<Form className={scss.form}>
-						<h2 className={scss.title}>{props.title}</h2>
-						<div className={scss.input__wrapper}>
-							<InputForm
-								autofocus="autofocus"
-								name="email"
-								type="email"
-								placeholder="Email"
-								autoComplete="off"
-							/>
-							<ErrorMessage
-								name="email"
-								component="p"
-								className={scss.error}
-							/>
-						</div>
-						<div className={scss.input__wrapper_last}>
-							<InputForm
-								name="password"
-								type={passwordShow ? 'text' : 'password'}
-								placeholder="Password"
-								autoComplete="off"
-							/>
-							<span
-								id="visibilityBtn"
-								className={scss.IconPassword}
-								onClick={togglePassword}
-							>
-								{passwordShow ? (
-									<VisibilityIcon />
-								) : (
-									<VisibilityOffIcon />
-								)}
-							</span>
-							<ErrorMessage
-								name="password"
-								component="p"
-								className={scss.error__password}
-							/>
-						</div>
+  return (
+    <div className={scss.container}>
+      <Formik validationSchema={loginValidationSchema} initialValues={initialValues} onSubmit={handleSubmit}>
+        {() => (
+          <Form className={scss.form}>
+            <h2 className={scss.title}>{props.title}</h2>
+            <div className={scss.input__wrapper}>
+              <InputForm autofocus="autofocus" name="email" type="email" placeholder="Email" autoComplete="off" />
+              <ErrorMessage name="email" component="p" className={scss.error} />
+            </div>
+            <div className={scss.input__wrapper_last}>
+              <InputForm name="password" type={passwordShow ? 'text' : 'password'} placeholder="Password" autoComplete="off" />
+              <span id="visibilityBtn" className={scss.IconPassword} onClick={togglePassword}>
+                {passwordShow ? <VisibilityIcon /> : <VisibilityOffIcon />}
+              </span>
+              <ErrorMessage name="password" component="p" className={scss.error__password} />
+            </div>
 
-						<div className={scss.button__container}>
-							<Button
-								type="submit"
-								className={scss.button__auth}
-								buttonName="Login"
-							></Button>
-						</div>
+            <div className={scss.button__container}>
+              <Button type="submit" className={scss.button__auth} buttonName="Login" onClick={play}></Button>
+            </div>
 
-						{isError && (
-							<p className={scss.error__login}>
-								{isError.message}
-							</p>
-						)}
-						{isError && (
-							<p className={scss.error__login}>
-								{isError.additionalInfo}
-							</p>
-						)}
-						<p className={scss.redirect__auth}>
-							Don't have an account?
-							<Link
-								to="/register"
-								className={scss.redirect_link__auth}
-							>
-								Register
-							</Link>
-						</p>
-						<p className={scss.redirect__auth}>
-							<Link
-								to="/forgot-password"
-								className={scss.redirect_link__auth}
-							>
-								Forgot password?
-							</Link>
-						</p>
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
+            {isError && <p className={scss.error__login}>{isError.message}</p>}
+            {isError && <p className={scss.error__login}>{isError.additionalInfo}</p>}
+            <p className={scss.redirect__auth}>
+              Don't have an account?
+              <Link to="/register" className={scss.redirect_link__auth}>
+                Register
+              </Link>
+            </p>
+            <p className={scss.redirect__auth}>
+              <Link to="/forgot-password" className={scss.redirect_link__auth}>
+                Forgot password?
+              </Link>
+            </p>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
