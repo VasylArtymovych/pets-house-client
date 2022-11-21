00382bfb7b1e7b3c9e8da4589a6fc95bfa0f88a8
Sergey-Kapitanchuk <kapitanchukserg28@gmail.com>
2022-11-22 00:32:42 +0200

    видалив лишнє

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,16 +1,12 @@
 import NewsList  from 'components/News/NewsList';
 import FormSearchNews from "components/News/NewsFormSearch";
-import { useState } from "react";
 
 
 function NewsPage() {
-const [filterNews, setFilterNews] = useState('');
-
-
     return (
         <div>
-            <FormSearchNews setFilter={setFilterNews}/>
-            <NewsList filter={filterNews}/>
+            <FormSearchNews/>
+            <NewsList/>
         </div>
     )
 };
