1d1ede3affba32a2a4f357433a7c9133a2602997
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-11-21 02:38:25 +0200

    many fixes

--- src/components/Notices/LearnMoreModal/LearnMoreModal.jsx
+++ src/components/Notices/LearnMoreModal/LearnMoreModal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import scss from './LearnMoreModal.module.scss';
 import Button from 'components/Button';
 import style from '../../Button/Button.module.scss';
+import { useAddToFavoritesMutation, useDeleteFromFavoritesMutation, useDeleteUserNoticeByIdMutation } from '../../../redux/fetchNotice';
 
 import sprite from '../../../images/symbol-defs.svg';
 
@@ -21,60 +22,95 @@ const LearnMore = ({
   favorite,
   myads
 }) => {
+  const [addToFavorites] = useAddToFavoritesMutation();
+  const [deleteFromFavorites] = useDeleteFromFavoritesMutation();
+  // const [deleteUserNoticeById] = useDeleteUserNoticeByIdMutation();
+
+  const handleAddToFavorites = () => {
+    addToFavorites(_id);
+  };
+  const handleDeleteFromFavorites = () => {
+    deleteFromFavorites(_id);
+  };
+
   return (
     <>
       <div className={scss.wrap_container}>
         <div className={scss.wrap_container_padding}>
-          <div className={scss.image}>
-            <img className={scss.photo} src={imageUrl} alt="Cat error" />
-            <div className={scss.sale}>
-              <span className={scss.text}>{category}</span>
+          <div className={scss.wrap_image_info}>
+            <button className={scss.button__close}>
+              <svg className={scss.iconClose}>
+                <use href={sprite + '#icon-exit'} />
+              </svg>
+            </button>
+            <div className={scss.image}>
+              <img className={scss.photo} src={imageUrl} alt="Cat error" />
+              <div className={scss.sale}>
+                <span className={scss.text}>{category}</span>
+              </div>
+            </div>
+
+            <div className={scss.wrap_info}>
+              <h3 className={scss.title}>{title}</h3>
+              <div className={scss.wrap_info_ul}>
+                <div className={scss.wrap_info_first_column}>
+                  <ul>
+                    <li className={scss.first_column}>Name:</li>
+                    <li className={scss.first_column}>Birthday:</li>
+                    <li className={scss.first_column}>Breed:</li>
+                    <li className={scss.first_column}>Place:</li>
+                    <li className={scss.first_column}>The sex:</li>
+                    <li className={scss.first_column}>Email :</li>
+                    <li className={scss.first_column}>Phone:</li>
+                    {category === 'Sell' && <li className={scss.first_column}>Sell:</li>}
+                  </ul>
+                </div>
+                <div>
+                  <ul>
+                    <li className={scss.second_column}>{name}</li>
+                    <li className={scss.second_column}>{age}</li>
+                    <li className={scss.second_column}>{breed}</li>
+                    <li className={scss.second_column}>{place}</li>
+                    <li className={scss.second_column}>{sex}</li>
+                    <li className={scss.second_column}>user@mail.com </li>
+                    <li className={scss.second_column}>+380971234567</li>
+                    {category === 'Sell' && <li className={scss.first_column}>{price}$</li>}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className={scss.wrap_info}>
-            <h3 className={scss.title}>{title}</h3>
-            <div className={scss.wrap_info_ul}>
-              <div className={scss.wrap_info_first_column}>
-                <ul>
-                  <li className={scss.first_column}>Name:</li>
-                  <li className={scss.first_column}>Birthday:</li>
-                  <li className={scss.first_column}>Breed:</li>
-                  <li className={scss.first_column}>Place:</li>
-                  <li className={scss.first_column}>The sex:</li>
-                  <li className={scss.first_column}>Email :</li>
-                  <li className={scss.first_column}>Phone:</li>
-                  {category === 'Sell' && <li className={scss.first_column}>Sell:</li>}
-                </ul>
-              </div>
-              <div>
-                <ul>
-                  <li className={scss.second_column}>{name}</li>
-                  <li className={scss.second_column}>{age}</li>
-                  <li className={scss.second_column}>{breed}</li>
-                  <li className={scss.second_column}>{place}</li>
-                  <li className={scss.second_column}>{sex}</li>
-                  <li className={scss.second_column}>user@mail.com </li>
-                  <li className={scss.second_column}>+380971234567</li>
-                  {category === 'Sell' && <li className={scss.first_column}>{price}$</li>}
-                </ul>
-              </div>
+          <div className={scss.wrap_coments_buttons}>
+            <div>
+              <p className={scss.comments}>Comments: {comments}</p>
             </div>
-          </div>
-          <div>
-            <p className={scss.comments}>Comments: {comments}</p>
-          </div>
-          <div className={scss.buttons}>
-            <Button
-              customStyle={style.button__auth_last + ' ' + style.botton__learn_more_mobile}
-              buttonName="Add to"
-              buttonIcon={
-                <svg className={scss.iconHeart}>
-                  <use href={sprite + '#icon-heartFull'} />
-                </svg>
-              }
-            ></Button>
-            <Button customStyle={style.botton__learn_more_mobile} buttonName="Contact"></Button>
+            <div className={scss.buttons}>
+              {favorite ? (
+                <Button
+                  customStyle={style.button__auth_last + ' ' + style.botton__learn_more_mobile}
+                  buttonName="Remove"
+                  buttonIcon={
+                    <svg className={scss.iconHeart}>
+                      <use href={sprite + '#icon-heartFull'} />
+                    </svg>
+                  }
+                  onClick={handleDeleteFromFavorites}
+                ></Button>
+              ) : (
+                <Button
+                  customStyle={style.button__auth_last + ' ' + style.botton__learn_more_mobile}
+                  buttonName="Add to"
+                  buttonIcon={
+                    <svg className={scss.iconHeart}>
+                      <use href={sprite + '#icon-heartEmpty'} />
+                    </svg>
+                  }
+                  onClick={handleAddToFavorites}
+                ></Button>
+              )}
+              <Button customStyle={style.botton__learn_more_mobile} buttonName="Contact"></Button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -4,9 +4,13 @@ import NoticeCategoryItem from 'components/Notices/NoticeCategoryItem';
 import styles from './NoticesCategoriesList.module.scss';
 import { useGetNoticeQuery, useGetNoticeFavoritesQuery, useGetUserNoticesQuery } from 'redux/fetchNotice';
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectors } from '../../../redux/selectors.js';
 
 const NoticesCategoriesList = () => {
   const { pathname } = useLocation();
+  const userFavorites = useSelector(selectors.getFavorites);
+  const userAds = useSelector(selectors.getUserNotices);
 
   const renderCategory = () => {
     switch (pathname) {
@@ -50,6 +54,7 @@ const NoticesCategoriesList = () => {
           setPets(favorites);
           return;
         }
+        console.log(favorites.favorites);
         setPets(favorites.favorites);
         return;
       } else {
@@ -58,6 +63,7 @@ const NoticesCategoriesList = () => {
           setPets(userNotices);
           return;
         }
+        console.log(userNotices.notices);
         setPets(userNotices.notices);
         return;
       }
@@ -84,40 +90,30 @@ const NoticesCategoriesList = () => {
       {pets && pets.length !== 0 ? (
         <ul className={styles.NoticesCategoriesList}>
           {pets.map(
-            ({
-              _id,
-              name,
-              owner,
-              comments = 'There is no comments',
-              sex,
-              category,
-              petImage,
-              title,
-              breed,
-              location,
-              dateOfBirth,
-              price,
-              favorite,
-              myads
-            }) => (
-              <NoticeCategoryItem
-                key={_id}
-                _id={_id}
-                name={name}
-                owner={owner}
-                sex={sex}
-                comments={comments}
-                category={category}
-                imageUrl={`http://localhost:8888/${petImage}`}
-                title={title}
-                breed={breed}
-                place={location}
-                price={price}
-                age={dateOfBirth}
-                favorite={favorite}
-                myads={myads}
-              />
-            )
+            ({ _id, name, owner, comments = 'There is no comments', sex, category, petImage, title, breed, location, dateOfBirth, price }) => {
+              const favorite = userFavorites.includes(_id);
+              const myads = userAds.includes(_id);
+
+              return (
+                <NoticeCategoryItem
+                  key={_id}
+                  _id={_id}
+                  name={name}
+                  owner={owner}
+                  sex={sex}
+                  comments={comments}
+                  category={category}
+                  imageUrl={`http://localhost:8888/${petImage}`}
+                  title={title}
+                  breed={breed}
+                  place={location}
+                  price={price}
+                  age={dateOfBirth}
+                  myads={myads}
+                  favorite={favorite}
+                />
+              );
+            }
           )}
         </ul>
       ) : (
