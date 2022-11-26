bcd69c54784cc5560603698eb16307bdbc1850dc
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-26 22:24:54 +0200

    add Theme

--- src/components/Header/Navigations.jsx
+++ src/components/Header/Navigations.jsx
@@ -14,7 +14,7 @@ import { selectors } from '../../redux/selectors';
 import { Trans, withTranslation } from 'react-i18next';
 import { useTranslation } from 'react-i18next';
 import ChangeLanguage from 'components/ChangeLanguage';
-import ChangeTheme from 'components/ChangeTheme/ChangeTheme';
+import ChangeTheme from 'components/ChangeTheme';
 
 const styleObjForHeaderMenuAndBurgerMenu = {
   styleNavigation,
