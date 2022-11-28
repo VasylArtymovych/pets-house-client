b67b7ccec23fbbe6dba138e2b2175ba388e4ddb4
VasylArtymovych <art777vasss@gmail.com>
2022-11-28 07:01:25 +0200

    update a bit form validation

--- src/services/addNoticeValidation.js
+++ src/services/addNoticeValidation.js
@@ -4,29 +4,29 @@ export const stepOneValidationSchema = Yup.object({
   title: Yup.string()
     .label('Title')
     .required('Title is required')
-    .matches(/^[a-zA-Z\s]+$/iu, 'In Latin letters'),
+    .matches(/^[a-zA-Z\s]+\.?$/iu, 'Only Latin letters'),
   name: Yup.string()
     .label('Name')
     .required('Name is required')
-    .matches(/^[a-zA-Z]+$/, 'In Latin letters without space'),
+    .matches(/^[a-zA-Z]+$/, 'Only Latin letters no space'),
   dateOfBirth: Yup.string()
     .matches(
       /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
-      'Correct format: 11.11.2022'
+      'Correct format: dd.mm.yyyy'
     )
     .label('Date of birth')
     .required('Date of birth is required'),
   breed: Yup.string()
     .label('Breed')
     .required('Breed is required')
-    .matches(/^[a-zA-Z\s]+$/iu, 'In Latin letters')
+    .matches(/^[a-zA-Z\s]+$/iu, 'Only Latin letters')
 });
 
 export const stepTwoValidationSchema = Yup.object({
   location: Yup.string()
     .required('Location is required')
     .label('Location')
-    .matches(/^[a-zA-Z\s]+$/iu, 'In Latin letters'),
+    .matches(/^[a-zA-Z\s,]+$/iu, 'Only Latin letters'),
   price: Yup.string()
     .label('Price')
     .matches(/^[1-9][0-9]*$/, 'Not start with 0')
