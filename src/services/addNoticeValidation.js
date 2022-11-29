ec66c25028c01bbfd9380fb2161328aea1f62e72
VasylArtymovych <art777vasss@gmail.com>
2022-11-29 10:31:44 +0200

    update Date validation

--- src/services/addNoticeValidation.js
+++ src/services/addNoticeValidation.js
@@ -9,14 +9,25 @@ export const stepOneValidationSchema = Yup.object({
     .label('Name')
     .required('Name is required')
     .matches(/^[a-zA-Z]+$/, 'Only Latin letters no space'),
-  dateOfBirth: Yup.date()
-    .max(new Date(), 'Enter valid date')
-    // .matches(
-    //   /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
-    //   'Correct format: dd.mm.yyyy'
-    // )
+  dateOfBirth: Yup.string()
+    .matches(
+      /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
+      'Correct format: dd.mm.yyyy'
+    )
+    .test(
+      'is-date-valid',
+      () => `Future date not allowed`,
+      (value) => {
+        let date = value.split('.');
+        const corectFormat = new Date(`${date[2]}/${date[1]}/${date[0]}`);
+        return corectFormat.getTime() < Date.now();
+      }
+    )
     .label('Date of birth')
     .required('Date of birth is required'),
+  // Yup.date()
+  //   .typeError('Date format: dd.mm.yyyy')
+  //   .max(new Date(), 'Enter valid date')
   breed: Yup.string()
     .label('Breed')
     .required('Breed is required')
@@ -27,7 +38,7 @@ export const stepTwoValidationSchema = Yup.object({
   location: Yup.string()
     .required('Location is required')
     .label('Location')
-    .matches(/^[a-zA-Z]+,\s[a-zA-Z]+$/iu, 'Only: City, Region'),
+    .matches(/^[a-zA-Z]+,\s[a-zA-Z]+$/iu, 'Format: City, Region'),
   price: Yup.string()
     .label('Price')
     .matches(/^[1-9][0-9]*$/, 'Not start with 0')
--- src/services/addPetValidation.js
+++ src/services/addPetValidation.js
@@ -1,11 +1,25 @@
 import * as Yup from 'yup';
 
 export const stepOneValidationSchema = Yup.object({
-  name: Yup.string().min(2, 'Too Short!').max(16, 'Too Long!').label('Name').required('Name is required'),
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(16, 'Too Long!')
+    .label('Name')
+    .required('Name is required')
+    .matches(/^[a-zA-Z\s]+?$/iu, 'Only Latin letters'),
   dateOfBirth: Yup.string()
     .matches(
       /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
       'Correct format: dd.mm.yyyy'
+    )
+    .test(
+      'is-date-valid',
+      () => `Future date not allowed`,
+      (value) => {
+        let date = value.split('.');
+        const corectFormat = new Date(`${date[2]}/${date[1]}/${date[0]}`);
+        return corectFormat.getTime() < Date.now();
+      }
     )
     .label('Date of birth')
     .required('Date of birth is required'),
